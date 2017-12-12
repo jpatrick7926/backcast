e18b136c778872db0eb1a05806375e720384fcc3
Hack Reactor Student <student@hackreactor.com>
2017-12-11 19:58:51 -0800

    got the appView to work

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,23 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+
+    //// Experiments ////
+    this.search = new SearchView();
+
+    //// This works, don't touch this ////
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    //// Experiments ////
+    this.search.render();
+
+    //// Originally here ////
     return this;
   },
 
